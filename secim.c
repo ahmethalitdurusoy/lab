f6f2fafffc75cc1c5071ec08ca14c056a1e05f3d
Mika Iwakura <can0zdmr@protonmail.com>
2016-05-25 22:41:19 +0300

    secim.c added

---
+++ secim.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	 
+	int yas;
+	printf("Yasini gir:");
+	scanf("%d", &yas);
+	
+	if(yas >= 18){
+		printf("Oy kullanma vaktin gelmis gocccum, Hadi sandiga!\n");
+	} else {		
+		printf("Git cakilda oyna!\n");
+	}
+	  
+}
