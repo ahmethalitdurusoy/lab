fedc12acff6952fc91710a3506fea45d119a8e3c
roissy <canozd07@gmail.com>
2016-06-06 18:05:48 +0300

    Update file_permissons.c

--- file_permissons.c
+++ file_permissons.c
@@ -9,13 +9,13 @@
 int main(int argc, char **argv)
 {
     char mod[] = "0777";
-    char yigin[100] = "/home/roissy/test.txt";
+    char yol[100] = "/home/roissy/test.txt";
     int i;
     i = strtol(mod, 0, 8);
-    if (chmod (yigin,i) < 0)
+    if (chmod (yol,i) < 0)
     {
         fprintf(stderr, "%s: hata(%s, %s) - %d (%s)\n",
-                argv[0], yigin, mod, errno, strerror(errno));
+                argv[0], yol, mod, errno, strerror(errno));
         exit(1);
     }
 
